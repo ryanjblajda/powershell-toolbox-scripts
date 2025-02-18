80506f2e6634c3ad1e776f7924cf1a6494a797c5
Ryan Blajda <ryan.j.blajda@gmail.com>
2025-02-18 15:30:27 -0500

    adjust

--- _powershell_discovered_device_command_invoker.ps1
+++ _powershell_discovered_device_command_invoker.ps1
@@ -8,8 +8,6 @@ catch
     Write-Warning "Run the _powershell_crestron_setup script you must!! - Yoda, probably..."
     Write-Error $_
 }
-
-
 function Get-Flattened {
     param([string]$ToFlatten)
        
@@ -18,7 +16,6 @@ function Get-Flattened {
 
     return($result)
 }
-
 function Get-Devices {
     do {
         $showRetryExitPrompt = $false
@@ -53,16 +50,9 @@ function Get-Devices {
     return ($discovered)
 }
 
-$SetCommand = {
-    param(
-        [string]$dev,
-        [string]$cmd, 
-        [string]$usr,
-        [string]$pw
-    )
-
-    Write-Host "Invoking Command on Device @ $($dev) // $cmd"
-    Invoke-CrestronCommand -Device $dev -Command $cmd -Username $usr -Password $pw -Secure
+$SendCommand = {
+    param([PSCustomObject]$dev, [string]$cmd, [string]$usr, [string]$pw)
+    Write-Host $dev
 }
 
 #loop our entire main loop for updating stuff often.
@@ -72,23 +62,28 @@ do {
     #display the output from Get-Devices
     Write-Output $discoveredCrestronDevices | Out-Default
 
+    #get the desired devices to configure
     do {
         #get the list of devices that the user wants to configure
         $response = Read-Host -Prompt "Enter the hostnames of the devices you wish to update/configure, separated by a comma"
         #get rid of the hidden crap we dont want
         $response -replace "`r`n", "" > $null
         #get rid of any spaces, and split the entries based on the comma to create a list
-        $targetdevices = $response.Replace(" ", "").Split(",")
+        $targetDevices = $response.Replace(" ", "").Split(",")
         #spit out a list to show the user exactly what devices we are going to configure so that we can make sure what they wanted is correct
         Write-Host "You wish to configure the following devices:" -ForegroundColor Yellow
         #loop through all the target devices
-        foreach($item in $targetdevices) {
-            $item = Get-Flattened($item)
+        $targetDeviceObjects = [System.Collections.ArrayList]::new()
+        foreach($item in $targetDevices) {
+            $item = Get-Flattened $item
             #find where we have a matching device in the discovered devices table
             $results = $discoveredCrestronDevices | Where-Object { $_.Hostname.ToLower() -eq $item.ToLower() }
             #if we find a matching result, print its details to the console
             if ($null -ne $results) {
                 $desiredDeviceIPAddress = $results[0].IP
+                #Write-Host $results[0]
+                $targetDeviceObjects.Add($results[0]) > $null
+                #Write-Host $targetDeviceObjects
                 Write-Host "$item @ $desiredDeviceIPAddress" -ForegroundColor DarkGreen
             }
         }
@@ -97,6 +92,7 @@ do {
     }
     while($desiredDevicesCorrect -ne "y")
 
+    #determine the credentials desired
     do {
         #store default credentials by default
         $username = "admin"
@@ -130,12 +126,74 @@ do {
 
     #we will ask the user how they wish to configure the devices, whether they want to send a batch of commands to all devices, or a unique command to each device
     do {
+        Write-Host "How would you like to send commands to devices?"
+        Write-Host "Enter [b] to send a single command to all devices [batch mode]" -ForegroundColor DarkGreen
+        Write-Host "Enter [u] to send a unique command to each device [unique mode]" -ForegroundColor Cyan
+        $response = Read-Host
+        $mode = Get-Flattened $response
+        $modeString = "Invalid!"
+        if ($mode -eq 'b') { $modeString = 'Batch'}
+        elseif ($mode -eq 'u') { $modeString = 'Unique'}
         
+        if ($mode -eq 'b' -or $mode -eq 'u') { 
+            Write-Host "You desire to send commands in $modeString mode?" -ForegroundColor Yellow
+            $response = Read-Host -Prompt "Enter [y] to confirm, [n] to choose a different option"
+            $modeConfirmed = Get-Flattened $response
+        }
+        else { 
+            Write-Warning -Message "Danger Will Robinson!`nYou've entered an invalid response. I know not what to do with this nonsense: $mode!!"
+        }
     }
-    while($commandsConfirmed -ne "y")
+    while($modeConfirmed -ne "y")
+
+    #batch mode stuff
+    if ($mode -eq 'b') {    
+        do {
+            #request the command from the user
+            Write-Host "Please enter the command you would like to send as a batch to all devices you've selected" -Foreground Cyan
+            $response = Read-Host
+            $command = Get-Flattened $response
+            #confirm the command entry
+            Write-Host "You want to send: $command to -> ALL DEVICES <-" -ForegroundColor Yellow
+            $response = Read-Host -Prompt "Enter [y] to confirm this command, [n] to re-type your desired command"
+            $commandConfirmed = Get-Flattened $response
+        }
+        while($commandConfirmed -ne "y")
+    }
+    elseif($mode -eq 'u') {
+        foreach ($device in $targetDeviceObjects) {
+            #Write-Host $device
+            do {
+                #request the command from the user
+                Write-Host "Please enter the command you would like to send to $($device.Hostname) @ $($device.IP)" -Foreground Cyan
+                $response = Read-Host
+                $command = Get-Flattened $response
+                #confirm the command entry
+                Write-Host "You want to send: $command to $($device.Hostname) @ $($device.IP)" -ForegroundColor Yellow
+                $response = Read-Host -Prompt "Enter [y] to confirm this command, [n] to re-type your desired command"
+                $commandConfirmed = Get-Flattened $response
+            }
+            while($commandConfirmed -ne "y")
+
+            $device | Add-Member -MemberType AliasProperty -Name Command -Value $command
+        }
+    }
 
     #for each command/device create a job
+    Write-Host "Beginning Jobs..." -Foreground DarkGreen
+
     $actions = {
+        if ($mode -eq "b") {
+            foreach ($device in $targetDeviceObjects) {
+                Start-Job -ScriptBlock $SendCommand -ArgumentList $device, $device.Command, $username, $password
+            } 
+        }
+        elseif ($mode -eq "u") {
+            foreach ($device in $targetDeviceObjects) {
+                Start-Job -ScriptBlock $SendCommand -ArgumentList $device, $device.Command, $username, $password
+            }
+        }
+
         Get-Job | Receive-Job -Wait
     }
 
